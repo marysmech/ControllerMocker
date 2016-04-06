f920a52f187e2831979eb458eab5eb06ed186c59
Marek Mechura <marysmech@gmail.com>
2016-04-07 00:17:38 +0200

    Initial Commit

---
+++ ControllerMocker/ControllerMocker.h
@@ -0,0 +1,19 @@
+//
+//  ControllerMocker.h
+//  ControllerMocker
+//
+//  Created by Marek Mechura on 07/04/16.
+//  Copyright © 2016 Marek Mechura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ControllerMocker.
+FOUNDATION_EXPORT double ControllerMockerVersionNumber;
+
+//! Project version string for ControllerMocker.
+FOUNDATION_EXPORT const unsigned char ControllerMockerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ControllerMocker/PublicHeader.h>
+
+
